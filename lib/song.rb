10c40c37009a91abe3787c156860cf942d260d4f
Musyoki <vincenbryaniel@gmail.com>
2022-08-29 10:57:56 +0300

    assignment completed

--- lib/song.rb
+++ lib/song.rb
@@ -1,25 +1,33 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
+
 class Song
-
-
   def self.table_name
     self.to_s.downcase.pluralize
   end
 
+      sql = "pragma table_info('#{table_name}')"
+  
+      table_info = DB[:conn].execute(sql)
+      column_names = []
+      
   def self.column_names
     DB[:conn].results_as_hash = true
+
 
     sql = "pragma table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
     column_names = []
+    
     table_info.each do |row|
       column_names << row["name"]
     end
+    
     column_names.compact
   end
+
 
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
@@ -37,10 +45,12 @@ class Song
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
+#Abstractig the table name
   def table_name_for_insert
     self.class.table_name
   end
 
+#Abstracting the column names
   def values_for_insert
     values = []
     self.class.column_names.each do |col_name|
@@ -59,6 +69,3 @@ class Song
   end
 
 end
-
-
-
